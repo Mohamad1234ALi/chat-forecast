4b3210d6309eee2a5f1c27ff8336da20c2118888
Mohamad Abed Al Houssein <125250165+Mohamad1234ALi@users.noreply.github.com>
2025-10-17 13:41:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-LAMBDA_API_URL = "https://03w3ig64hl.execute-api.us-east-1.amazonaws.com/default/chat-forecast-insight"
+LAMBDA_API_URL = "https://03w3ig64hl.execute-api.us-east-1.amazonaws.com/default/chat-forecast-insight/chat"
 
 st.title("📊 Forecast Insight Chatbot")
 
